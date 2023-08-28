9e0269fb57a6b4f810e5713d34823f0ed49cdc19
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2023-08-28 11:32:43 +0100

    evaluate interger and boolean expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -20,13 +20,25 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		if node.Value {
-			return True
-		}
-		return False
+		return evalBoolean(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+
+		return evalInfixExpression(node.Operator, left, right)
 	}
 
 	return nil
+}
+
+func evalBoolean(b bool) object.Object {
+	if b {
+		return True
+	}
+	return False
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
@@ -36,4 +48,75 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalExclaimOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	default:
+		return Null
+	}
+}
+
+func evalExclaimOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case True:
+		return False
+	case False:
+		return True
+	case Null:
+		return True
+	default:
+		return False
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.IntegerType {
+		return Null
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
+
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.IntegerType && right.Type() == object.IntegerType:
+		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return evalBoolean(left == right)
+	case operator == "!=":
+		return evalBoolean(left != right)
+	default:
+		return Null
+	}
+}
+
+func evalIntegerInfixExpression(operator string, lObj, rObj object.Object) object.Object {
+	left := lObj.(*object.Integer).Value
+	right := rObj.(*object.Integer).Value
+	switch operator {
+	case "+":
+		return &object.Integer{Value: left + right}
+	case "-":
+		return &object.Integer{Value: left - right}
+	case "*":
+		return &object.Integer{Value: left * right}
+	case "/":
+		return &object.Integer{Value: left / right}
+	case "<":
+		return evalBoolean(left < right)
+	case ">":
+		return evalBoolean(left > right)
+	case "==":
+		return evalBoolean(left == right)
+	case "!=":
+		return evalBoolean(left != right)
+	default:
+		return Null
+	}
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -19,6 +19,45 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}, {
 		input:    "10",
 		expected: 10,
+	}, {
+		input:    "-5",
+		expected: -5,
+	}, {
+		input:    "-10",
+		expected: -10,
+	}, {
+		input:    "5 + 5 + 5 + 5 -10",
+		expected: 10,
+	}, {
+		input:    "2 * 2 * 2 * 2 * 2",
+		expected: 32,
+	}, {
+		input:    "-50 + 100 + -50",
+		expected: 0,
+	}, {
+		input:    "5 * 2 + 10",
+		expected: 20,
+	}, {
+		input:    "5 + 2 * 10",
+		expected: 25,
+	}, {
+		input:    "20 + 2 * -10",
+		expected: 0,
+	}, {
+		input:    "50 / 2 * 2 + 10",
+		expected: 60,
+	}, {
+		input:    "2 * (5 + 10)",
+		expected: 30,
+	}, {
+		input:    "3 * 3 * 3 + 10",
+		expected: 37,
+	}, {
+		input:    "3 * (3 * 3) + 10",
+		expected: 37,
+	}, {
+		input:    "(5 + 10 * 2 + 15 / 3) * 2 + -10",
+		expected: 50,
 	}}
 
 	for _, test := range tests {
@@ -37,6 +76,87 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}, {
 		input:    "false",
 		expected: false,
+	}, {
+		input:    "1 < 2",
+		expected: true,
+	}, {
+		input:    "1 > 2",
+		expected: false,
+	}, {
+		input:    "1 < 1",
+		expected: false,
+	}, {
+		input:    "1 > 1",
+		expected: false,
+	}, {
+		input:    "1 == 1",
+		expected: true,
+	}, {
+		input:    "1 != 1",
+		expected: false,
+	}, {
+		input:    "1 == 2",
+		expected: false,
+	}, {
+		input:    "1 != 2",
+		expected: true,
+	}, {
+		input:    "true == true",
+		expected: true,
+	}, {
+		input:    "false == false",
+		expected: true,
+	}, {
+		input:    "true == false",
+		expected: false,
+	}, {
+		input:    "true != false",
+		expected: true,
+	}, {
+		input:    "false != true",
+		expected: true,
+	}, {
+		input:    "(1 < 2) == true",
+		expected: true,
+	}, {
+		input:    "(1 < 2) == false",
+		expected: false,
+	}, {
+		input:    "(1 > 2) == true",
+		expected: false,
+	}, {
+		input:    "(1 > 2) == false",
+		expected: true,
+	}}
+
+	for _, test := range tests {
+		evaluated := testEval(test.input)
+		testBooleanObject(t, evaluated, test.expected)
+	}
+}
+
+func TestExclaimOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{{
+		input:    "!true",
+		expected: false,
+	}, {
+		input:    "!false",
+		expected: true,
+	}, {
+		input:    "!5",
+		expected: false,
+	}, {
+		input:    "!!true",
+		expected: true,
+	}, {
+		input:    "!!false",
+		expected: false,
+	}, {
+		input:    "!!5",
+		expected: true,
 	}}
 
 	for _, test := range tests {
@@ -67,7 +187,7 @@ func testIntegerObject(t *testing.T, o object.Object, expected int64) bool {
 func testBooleanObject(t *testing.T, o object.Object, expected bool) bool {
 	result, ok := o.(*object.Boolean)
 	if !ok {
-		t.Errorf("object is not Integer. got=%T (%+v)", o, o)
+		t.Errorf("object is not Boolean. got=%T (%+v)", o, o)
 		return false
 	}
 
